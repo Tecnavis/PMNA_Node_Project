bdf69d25406eee178f1d8f140393462d4a69e4e1
sajna <sajnaaizanaufal@gmail.com>
2025-03-18 10:30:59 +0530

    Added new feature

--- RSA Frontend/RSA Frontend/src/pages/Reports/StaffReport.tsx
+++ RSA Frontend/RSA Frontend/src/pages/Reports/StaffReport.tsx
@@ -124,7 +124,6 @@ const StaffReport = () => {
                   </div>
                 ),
               },
-            ggggggggggggggggggggggggggggggggggggggggggg
               
               {
                 accessor: 'cashInHand',
