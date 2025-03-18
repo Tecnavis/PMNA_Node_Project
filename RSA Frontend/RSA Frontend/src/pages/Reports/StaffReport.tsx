2358561da51f7711997082314bd9898ad56b1e7f
sajna <sajnaaizanaufal@gmail.com>
2025-03-18 10:52:54 +0530

    Added new feature

--- RSA Frontend/RSA Frontend/src/pages/Reports/StaffReport.tsx
+++ RSA Frontend/RSA Frontend/src/pages/Reports/StaffReport.tsx
@@ -56,7 +56,7 @@ const StaffReport = () => {
       console.error('Error fetching staffs:', error);
     }
   };
-
+to check git branch
   // Check token and fetch data
   useEffect(() => {
     const token = localStorage.getItem('token');
