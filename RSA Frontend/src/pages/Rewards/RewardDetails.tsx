c0732c4a232ff9838b96ff7bb5cf7332d10ced61
hadhi hassan m <diluhadhi01@gmail.com>
2025-04-28 15:24:50 +0530

    feat: Rewards full details page completed

--- RSA Frontend/src/pages/Rewards/RewardDetails.tsx
+++ RSA Frontend/src/pages/Rewards/RewardDetails.tsx
@@ -178,6 +178,12 @@ const RewardDetails: React.FC = () => {
             {activeTab === 'available' ? (
               <div>
                 <h3 className="text-xl font-semibold text-gray-800 mb-6">Redeem Your Points</h3>
+                {
+                  redemableRewards.length < 1 &&  <div className="text-center py-12">
+                  <FiClock className="mx-auto text-4xl text-gray-400 mb-4" />
+                  <p className="text-gray-500">No redemable Rewards</p>
+                </div>
+                }
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                   {redemableRewards?.map((reward) => (
                     <motion.div
