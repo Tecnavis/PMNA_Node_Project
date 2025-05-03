e4dcf2cd2f59c8a1bf92eef1058f2c72343abdb4
hadhi hassan m <diluhadhi01@gmail.com>
2025-05-03 16:19:22 +0530

    unwanted import removed.

--- rsa-staff-dashboard/src/pages/Users/DriverDetails.tsx
+++ rsa-staff-dashboard/src/pages/Users/DriverDetails.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const DriverDetails = () => {
 
