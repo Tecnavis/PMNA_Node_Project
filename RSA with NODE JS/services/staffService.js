c6bca0fb583273885435658740a89e52839a32b2
sajna <sajnaaizanaufal@gmail.com>
2025-06-14 11:33:34 +0530

    staffservice

--- RSA with NODE JS/services/staffService.js
+++ RSA with NODE JS/services/staffService.js
@@ -2,30 +2,38 @@ const Staff = require('../Model/staff');
 const Booking = require('../Model/booking');
 
 async function calculateNetTotalAmountInHand(staffId) {
-    const result = await Booking.aggregate([
-        {
-            $match: {
-                receivedUserId: staffId,
-                receivedUser:Staff,
-                cashPending: false,
-                status: 'Order Completed',
-                workType: 'PaymentWork'
-            }
-        },
-              {
-            $group: {
-                _id: null,
-                netTotalAmount: { 
-                    $sum: { 
-                        $subtract: ['$receivedAmountStaff', '$givenAmountByStaff'] 
-                    } 
+    try {
+        const result = await Booking.aggregate([
+            {
+                $match: {
+                    receivedUserId: staffId,
+                    receivedUser: 'Staff', // as a string
+                    cashPending: false,
+                    status: 'Order Completed',
+                    workType: 'PaymentWork'
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    netTotalAmount: {
+                        $sum: {
+                            $subtract: [
+                                '$receivedAmountStaff',
+                                { $ifNull: ['$givenAmountByStaff', 0] }
+                            ]
+                        }
+                    }
                 }
             }
-        }
-    ]);
-    return result[0]?.netTotalAmount || 0;
-}
+        ]);
 
+        return result[0]?.netTotalAmount || 0;
+    } catch (error) {
+        console.error('Error calculating net total amount:', error);
+        return 0; // Return 0 in case of error
+    }
+}
 module.exports = {
     calculateNetTotalAmountInHand
 }
