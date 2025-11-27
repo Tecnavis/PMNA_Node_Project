bd53bd4d55273300ee2a428e495d92a4a7566e63
sajna <sajnaaizanaufal@gmail.com>
2025-11-27 12:37:10 +0530

    pagination

--- RSA Frontend/src/pages/Reports/CompanyReport/PendingPayment.tsx
+++ RSA Frontend/src/pages/Reports/CompanyReport/PendingPayment.tsx
@@ -1011,7 +1011,7 @@ const fetchBookings = async () => {
                             recordsPerPage={pageSize}
                             page={page}
                             onPageChange={(p) => setPage(p)}
-                            recordsPerPageOptions={[10, 20, 50,100,200]}
+                            recordsPerPageOptions={[10, 20, 50,100,200,500]}
                             onRecordsPerPageChange={(newPageSize) => {
                                 setPageSize(newPageSize);
                                 setPage(1);
--- RSA Frontend/src/pages/Reports/ProviderReport/ProviderReport.tsx
+++ RSA Frontend/src/pages/Reports/ProviderReport/ProviderReport.tsx
@@ -725,7 +725,7 @@ const ProviderReport = () => {
                             recordsPerPage={pageSize}
                             page={page}
                             onPageChange={(p) => setPage(p)}
-                            recordsPerPageOptions={[10, 20, 50]}
+                            recordsPerPageOptions={[10, 20, 50,100,200,500]}
                             onRecordsPerPageChange={(newPageSize) => {
                                 setPageSize(newPageSize);
                                 setPage(1);
