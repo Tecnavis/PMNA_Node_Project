9e7f4dad44d7553da563e1b1c75478a88917d78e
hadhihassan <diluhadhi01@gmail.com>
2025-11-27 22:01:17 +0530

    fix: Get Qr image added more role access.

--- RSA with NODE JS/Controller/admin.js
+++ RSA with NODE JS/Controller/admin.js
@@ -97,13 +97,8 @@ exports.updateAdminQR = async (req, res) => {
 
 exports.getAdminQR = async (req, res) => {
   try {
-    const adminId = req.user?.id;
-    const admin = await Admin.findById(adminId);
-
-    if (!admin) {
-      return res.status(404).json({ message: "Admin not found" });
-    }
-
+    const admin = await Admin.findOne().select("qrImage -_id");
+    
     return res.status(200).json({
       qrImage: admin.qrImage?.url || null,
     });
